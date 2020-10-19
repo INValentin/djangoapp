08a2f53b1f52704e6c1f6096f2978eae55798491
INValentin <57767759+INValentin@users.noreply.github.com>
2020-10-19 03:41:20 +0200

    Add files via upload

---
+++ candoit/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', include('posts.urls')),
+    path('', include('accounts.urls')),
+    path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
