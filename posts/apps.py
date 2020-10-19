da6a1035e62d074df2e054afc4c57d1e78767f02
INValentin <57767759+INValentin@users.noreply.github.com>
2020-10-19 03:48:30 +0200

    Add files via upload

--- posts/apps.py
+++ posts/apps.py
@@ -3,6 +3,9 @@ from django.apps import AppConfig
 
 class PostConfig(AppConfig):
     name = 'posts'
-    
+
     def ready(self):
-        from . import signals 
+    	try:
+	        from posts import signals
+	    except ImportError:
+	    	pass
